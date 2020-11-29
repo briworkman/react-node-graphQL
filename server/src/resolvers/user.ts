29a720f0da88b8688164610ab04c95a0e848c9ba
Brianna Workman <brianna.workman@nerdery.com>
2020-11-28 19:20:15 -0500

    adds error handling to user register mutation in user resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -32,15 +32,45 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: 'length must be greateer than 2'
+                }]
+            }
+        }
+
+        if (options.password.length <= 2) {
+            return {
+                errors: [{
+                    field: 'passowrd',
+                    message: 'length must be greateer than 2'
+                }]
+            }
+        }
+
         const hashedPassword = await argon2.hash(options.password)
         const user = em.create(User, { username: options.username, password: hashedPassword })
-        await em.persistAndFlush(user);
-        return user;
+        try {
+            await em.persistAndFlush(user);
+        } catch (err) {
+            if (err.code === '23505') {
+                return {
+                    errors: [{
+                        field: 'username',
+                        message: 'username has already been taken'
+                    }]
+                }
+            }
+            console.log("message: ", err.message)
+        }
+        return { user };
     }
 
     @Mutation(() => UserResponse)
