66e4b28fd1ab37e78f81bbd10b434efdc632f3f0
Brianna Workman <brianna.workman@nerdery.com>
2020-12-14 14:06:11 -0500

    sets up a many to one relationship between users and posts, and sets up auth to ensure a user cannot post unless they are logged in

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
 
-export type MyContext{
+export type MyContext = {
     req: Request & { session: Express.Session };
     redis: Redis;
     res: Response;
